4671983f4a797c8c65c328c9579a141d3d99885b
Yali Bayer <yalibayer@gmail.com>
2026-05-11 20:33:36 +0300

    pdf download

--- resume_project/urls.py
+++ resume_project/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import resume_view
+from . import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', resume_view, name='home'),
+    path('download-resume/', views.download_resume, name='download_resume'),
+
 ]
